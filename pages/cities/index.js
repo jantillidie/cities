a300528d9af1ab045089bd1af115e4292767fe42
Jan Maaß <git@antipost.de>
2023-11-23 12:57:45 +0100

    Add cities page and list

--- pages/cities/index.js
+++ pages/cities/index.js
@@ -1,1 +1,16 @@
 import { cities } from "@/lib/data";
+
+export default function CitiesOverview() {
+  return (
+    <>
+      <h1>Cities</h1>
+      <ul>
+        {cities.map(({ id, name }) => (
+          <li key={id}>
+            {name}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
